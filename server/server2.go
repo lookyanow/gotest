a29238d514956dc0cabbb130a61e75f664314e7e
Ivan Lookyanow <ivanlook@ivanlook.ru>
2016-10-24 18:10:36 +0300

    new server

--- server/server2.go
+++ server/server2.go
@@ -5,14 +5,9 @@ import "net/http"
 
 func main(){
 	http.HandleFunc("/", someFunc)
-	http.HandleFunc("/hello", hello)
 	http.ListenAndServe(":8080", nil)
 }
 
 func someFunc(w http.ResponseWriter, req *http.Request){
-	w.Write([]byte("Index responce from server\n"))
-}
-
-func hello(w http.ResponseWriter, req *http.Request){
-	w.Write([]byte("Hello world\n"))
+	w.Write([]byte("Hello world"))
 }
