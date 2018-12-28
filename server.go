0ebe5708dee68211665dc6aebc08feda15390635
Lukyanov Ivan <ilukyanov@ozon.ru>
2018-12-28 14:18:58 +0300

    test handler deleted

--- server.go
+++ server.go
@@ -11,7 +11,6 @@ import (
 func main(){
 	http.HandleFunc("/", someFunc)
 	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/test", test)
 	http.HandleFunc("/dump", getDump)
 	http.ListenAndServe(":80", nil)
 }
@@ -22,10 +21,6 @@ func someFunc(w http.ResponseWriter, req *http.Request){
 
 func hello(w http.ResponseWriter, req *http.Request){
 	w.Write([]byte("Hello world\n"))
-}
-
-func test(w http.ResponseWriter, req *http.Request){
-	w.Write([]byte("New pipeline test\n"))
 }
 
 func getDump(w http.ResponseWriter, r *http.Request) {
